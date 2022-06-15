f1d7c754867ff187b01b0d413503234abea9153d
akhmadullinaynur <akhmadullinaynur@yandex.ru>
2022-06-15 11:30:25 +0300

    Account.razor

--- WorkflowManagementSystem/Data/Client.cs
+++ WorkflowManagementSystem/Data/Client.cs
@@ -13,18 +13,21 @@ namespace WorkflowManagementSystem.Data
         [Required(ErrorMessage = "Необходимо заполнить это поле")]
         public string Password { get; set; }
 
-
         public string SurName { get; set; }
         public string Profession { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
         public bool Active { get; set; }
         public bool Admin { get; set; }
-        public void GetProfession()
+        public void GetUserData()
         {
             var client = MongoDataBase.FindUser(Name);
             foreach (var item in client)
             {
                 SurName = item.SurName;
                 Profession = item.Profession;
+                Email = item.Email;
+                PhoneNumber = item.PhoneNumber;
             }
         }
     }
